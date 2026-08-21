94eaf4abb0c215f0ea3ab34704524ee4440a8025
Prathamesh Zagade <prathamzagade1811@gmail.com>
2026-08-21 17:04:00 +0530

    python3

--- Beginner/011_Count_Vowels.py
+++ Beginner/011_Count_Vowels.py
@@ -4,3 +4,14 @@ Question 011
 Count the number of vowels in a given string.
 -------------
 """
+
+string = input("please enter a string: ")
+string = str.lower()
+str_count =0
+
+for i in string:
+    if i in "aeiou":
+        str_count += 1
+
+print(str_count)
+
